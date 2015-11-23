c928f7e9d5f74954f8d7d5c969b3b55474133447
sellis7 <sellis7@users.noreply.github.com>
2015-11-23 12:34:25 -0500

    Modification of quiz1app with proper database designations/Additional page added

--- app/controllers/user_costumes_controller.rb
+++ app/controllers/user_costumes_controller.rb
@@ -1,4 +1,24 @@
 class UserCostumesController < ApplicationController
   def index
+    @user_costume = UserCostume.new
   end
+
+  def new
+  end
+
+  def create
+    @user_costume = UserCostume.create(costume_params)
+    redirect_to user_costume_path(@user_costume)
+  end
+
+  def show
+    @user_costume = UserCostume.last
+  end
+
+  private
+
+	def costume_params
+		params.require(:user_costume).permit(:costume, :year)
+	end
+
 end
